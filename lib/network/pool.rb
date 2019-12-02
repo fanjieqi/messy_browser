2bb0330e8d2054d29ba02f18eb3639ba3c24c49b
fanjieqi <fjqqiqi@163.com>
2019-12-02 22:25:12 +0800

    Add basic Network::Pool & Network::Task

--- lib/network/pool.rb
+++ lib/network/pool.rb
@@ -3,5 +3,42 @@ require_relative 'task'
 
 module Network
   class Pool
+    TIMEOUT = 10
+    attr_reader :queue, :concurrent_size, :tasks, :threads
+
+    def initialize(params = {})
+      @queue = Queue.new
+      @concurrent_size = params[:concurrent_size]
+      @tasks = @concurrent_size.times.map { ::Network::Task.new(self) }
+      @threads = @tasks.map(&:run)
+    end
+
+    def push(object, params = {})
+      @queue << [object, params]
+    end
+
+    def get_thread_index
+      index = @threads.map.with_index { |thread, index| [thread.status, index] }
+                      .select { |status, index| status == ::Network::Task::SLEEP }
+                      .sample.last
+    end
+
+    def run
+      Thread.new {
+        loop do
+          next if @queue.empty?
+          index = get_thread_index
+          next if index.blank?
+
+          tag, params = @queue.pop
+          begin
+            @tasks[index].push(tag, params)
+          rescue => e
+            warn e.full_message
+            push(tag, params)
+          end
+        end
+      }
+    end
   end
 end
--- lib/network/task.rb
+++ lib/network/task.rb
@@ -2,5 +2,36 @@
 
 module Network
   class Task
+    TIMEOUT = 60
+    SLEEP = 'sleep'
+    attr_reader :pool, :queue
+
+    def initialize(pool)
+      @pool = pool
+      @queue = Queue.new
+    end
+
+    def push(object, params = {})
+      @queue << [object, params]
+    end
+
+    def run
+      Thread.new {
+        loop do
+          object, params = @queue.pop
+          next if object.blank? || params.blank?
+
+          begin
+            puts "#{Thread.current} is Working"
+            result = RestClient::Request.execute(params.merge(timeout: TIMEOUT))
+            object.process(result)
+          rescue => e
+            warn e.full_message
+          ensure
+            Thread.pass
+          end
+        end
+      }
+    end
   end
 end
